74d8782cc397926d2c2b4566fab6859c060f401a
KAT <104945136+KATT07@users.noreply.github.com>
2023-11-29 16:18:39 +0530

    Create battery-reporter.py

---
+++ battery-reporter.py
@@ -0,0 +1,19 @@
+import psutil
+import paho.mqtt.client as mqtt
+
+#Edit These to match your config
+broker_address="192.168.XXX.XXX"
+#username = 'XXX'
+#password = 'XXX'
+client_name="XXX" 
+port = 1883
+topic = "XXX/XXX"
+
+battery_status = psutil.sensors_battery()
+client = mqtt.Client(client_name) #create new instance
+#client.username_pw_set(username, password)
+client.connect(broker_address) #connect to broker
+
+print(battery_status)
+print(battery_status.percent)
+client.publish(topic,int(battery_status.percent)) #publish
