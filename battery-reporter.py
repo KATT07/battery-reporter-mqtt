ca711ccf513914e8d5fc36fee68a42501be5b54b
KAT <104945136+KATT07@users.noreply.github.com>
2025-02-25 14:37:09 +0530

    Update battery-reporter.py

--- battery-reporter.py
+++ battery-reporter.py
@@ -9,11 +9,15 @@ client_name = "XXX"
 port = 1883
 topic = "XXX/XXX"
 
+
 battery_status = psutil.sensors_battery()
 client = mqtt.Client(client_name)  # create new instance
 # client.username_pw_set(username, password)
 client.connect(broker_address)  # connect to broker
 
-print(battery_status)
-print(battery_status.percent)
-client.publish(topic, int(battery_status.percent))  # publish
+if battery_status is not None:
+  print(battery_status)
+  print(battery_status.percent)
+  client.publish(topic, int(battery_status.percent))  # publish
+else:
+  print("No Battery Found!")
